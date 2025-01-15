fbd53a471fa9066785320cf03388d6db0e058c31
Olavo Vales <olavovgcs@gmail.com>
2025-01-15 17:52:53 -0300

    questão 2 pronta

--- src/main/java/questao2.java
+++ src/main/java/questao2.java
@@ -6,6 +6,39 @@ public class questao2 {
 
         Scanner sc1 = new Scanner(System.in);
 
-        System.out.println("digite o numero que deseja saber se está ou não na sequencia de fibonacci");
+        System.out.print("digite o numero que deseja saber se está ou não na sequencia de fibonacci: ");
+        int numeroDigitado = sc1.nextInt();
+
+        if (ehFibonnaci(numeroDigitado)) {
+            System.out.println("O número " + numeroDigitado + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + numeroDigitado + " não pertence à sequência de Fibonacci.");
+        }
+
+        sc1.close();
+    }
+
+    static boolean ehFibonnaci (int numeroDigitado) {
+        if (numeroDigitado < 0){ return false; }
+
+        int primeiroNumero = 0;
+        int segundoNumero = 1;
+
+        if (numeroDigitado == primeiroNumero || numeroDigitado == segundoNumero) {
+            return true;
+        }
+
+        while(segundoNumero < numeroDigitado){
+            int numeroVariante = primeiroNumero + segundoNumero;
+            primeiroNumero = segundoNumero;
+            segundoNumero = numeroVariante;
+
+            if (segundoNumero == numeroDigitado){
+                return true;
+            }
+
+        }
+
+        return false;
     }
 }
