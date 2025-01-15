360202572fe875701c48c795481724526f476cf1
Olavo Vales <olavovgcs@gmail.com>
2025-01-15 17:24:53 -0300

    questão 1 realizada

---
+++ src/main/java/questao2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class questao2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc1 = new Scanner(System.in);
+
+        System.out.println("digite o numero que deseja saber se está ou não na sequencia de fibonacci");
+    }
+}
